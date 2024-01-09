f768e5f02c1be19b4f718232b361c0240ee45d91
HidenYT <buharev2009@gmail.com>
2024-01-09 16:17:01 +0300

    Added comments to keypoints module

--- app/video_labeling/keypoints.py
+++ app/video_labeling/keypoints.py
@@ -1,10 +1,12 @@
 from core.models.skeleton import SkeletonNode, Skeleton
-from typing import Dict, TYPE_CHECKING, List, Set
+from typing import Dict, TYPE_CHECKING, List, Set, Tuple
 
 if TYPE_CHECKING:
     from .labeling_canvas import LabelingCanvas
 
 class KeypointManager:
+    '''Эффетивно управляет множеством точек скелета и связанных с ними объектов. Хранит координаты каждой точки, id её объекта на Canvas, соответствующее имя в скелете, id объекта надписи точки на Canvas.'''
+    
     def __init__(self, canvas: "LabelingCanvas", skeleton: Skeleton | None = None) -> None:
         self.canvas = canvas
         self.skeleton = skeleton
@@ -15,16 +17,19 @@ class KeypointManager:
         self.kp_text_by_kp_id: Dict[int, int] = {}
     
     def get_keypoints_coordinates(self) -> Dict[str, tuple[float, float]]:
+        '''Возвращает координаты всех точек в словаре {название_точки: (xi, yi)}'''
         result = {}
         for key in self.keypoint_by_name:
             result[key] = self.get_keypoint_coordinates(self.keypoint_by_name[key])
         return result
 
-    def get_keypoint_coordinates(self, kp: "Keypoint"):
+    def get_keypoint_coordinates(self, kp: "Keypoint") -> Tuple[float, float]:
+        '''Возвращает координаты точки в виде пары xi, yi'''
         cont_x, cont_y = self.canvas.get_containter_top_left()
         return ((kp.x-cont_x)/self.canvas.imscale, (kp.y-cont_y)/self.canvas.imscale)
 
     def add_keypoint(self, kp_id: int, kp_name: str):
+        '''Добавляет точку в менеджер по id на Canvas и названию'''
         if self.skeleton is None: raise Exception("Trying to add keypoint to the KeypointManager, but skeleton was not provided.")
         node = self.skeleton.nodes[kp_name]
         kp = Keypoint(self.canvas, kp_id, node)
@@ -34,6 +39,11 @@ class KeypointManager:
         self.kp_id_by_name[kp_name] = kp_id
     
     def add_kp_text(self, kp: int | str, text_id: int):
+        '''Добавляет напись, соответствующую точке.
+        
+        - `kp` - id на Canvas или название точки;
+        
+        - `text_id` - id надписи на Canvas'''
         if isinstance(kp, str):
             id = self.kp_id_by_name[kp]
             self.kp_text_by_kp_id[id] = text_id
@@ -43,21 +53,29 @@ class KeypointManager:
             raise TypeError("kp can be only int or str.")
     
     def get_kp_by_name(self, name: str) -> "Keypoint":
+        '''Возвращает объект `Keypoint` по названию точки.'''
         return self.keypoint_by_name[name]
     
     def get_kp_by_id(self, id: int) -> "Keypoint":
+        '''Возвращает объект `Keypoint` по id точки на Canvas.'''
         return self.keypoint_by_id[id]
     
     def get_name_by_id(self, id: int) -> str:
+        '''Возвращает название точки по id точки на Canvas.'''
         return self.kp_name_by_id[id]
     
     def get_id_by_name(self, name: str) -> int:
+        '''Возвращает id точки по названию точки.'''
         return self.kp_id_by_name[name]
     
     def get_text_id_by_kp_id(self, id: int) -> int:
+        '''Возвращает id надписи по id точки на Canvas.'''
         return self.kp_text_by_kp_id[id]
     
     def remove_kp(self, id_or_name: str | int):
+        '''Удаляет точку из менеджера точек.
+        
+        - `id_or_name` - id точки на Canvas или название точки.'''
         if isinstance(id_or_name, int):
             id = id_or_name
             name = self.get_name_by_id(id)
@@ -72,6 +90,7 @@ class KeypointManager:
         del self.kp_id_by_name[name]
     
     def set_skeleton(self, skeleton: Skeleton):
+        '''Устанавливает скелет, используемый при работе с точками.'''
         self.skeleton = skeleton
         self.keypoint_by_id: Dict[int, Keypoint] = {}
         self.keypoint_by_name: Dict[str, Keypoint] = {}
@@ -80,12 +99,15 @@ class KeypointManager:
         self.kp_text_by_kp_id: Dict[int, int] = {}
     
     def get_kp_ids(self):
+        '''Возвращает `dict_keys`, содержащий id всех точек менеджера.'''
         return self.keypoint_by_id.keys()
     
     def get_text_ids(self) -> Set[int]:
+        '''Возвращает `set`, содержащий id всех надписей.'''
         return set(self.kp_text_by_kp_id.values())
     
     def clear(self):
+        '''Удаляет все точки и тексты из менеджера.'''
         self.keypoint_by_id: Dict[int, Keypoint] = {}
         self.keypoint_by_name: Dict[str, Keypoint] = {}
         self.kp_name_by_id: Dict[int, str] = {}
