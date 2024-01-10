3abdd56d434a9ee62183b3f19f2485d027f20450
HidenYT <buharev2009@gmail.com>
2024-01-10 14:26:08 +0300

    Added controls to video_frame

--- app/frames_selection/video_frame.py
+++ app/frames_selection/video_frame.py
@@ -1,3 +1,4 @@
+from shutil import move
 import tkinter as tk
 from tkinter import ttk
 from .video_frame_canvas import VideoFrameCanvas
@@ -8,6 +9,8 @@ if TYPE_CHECKING:
     from .frames_selection_controller import FramesSelectionController
 
 class VideoFrame(tk.Frame):
+    FRAME_CHANGE_DELTA = 1
+
     def __init__(self, 
                  master,
                  controller: "FramesSelectionController", 
@@ -43,32 +46,68 @@ class VideoFrame(tk.Frame):
 
     def setup_bottom_bar(self) -> tk.Frame:
         self.bottom_frame = frm = tk.Frame(self)
-        self.slider = slider = ttk.Scale(frm, from_=0, to=self.frames_n-1, value=0, command=self.on_slider_changed)
+        self.frame_n_label = tk.Label(frm, text="0")
+        self.frame_n_label.pack(fill='x')
+
+        frm_controls = self.create_controls_frame(frm)
+        frm_controls.pack(fill='x')
+
+        frm_playpause = self.create_playpause_frame(frm)
+        frm_playpause.pack(fill='x')
+
+        return frm
+    
+    def create_controls_frame(self, root) -> tk.Frame:
+        frm = tk.Frame(root)
+        self.btn_slider_left = ttk.Button(
+            frm, 
+            text="<", 
+            command=lambda: self.move_video_pos(-self.FRAME_CHANGE_DELTA)
+        )
+        self.btn_slider_left.pack(side='left', fill='x')
+
+        self.slider = slider = ttk.Scale(
+            frm, 
+            from_=0, 
+            to=self.frames_n-1, 
+            value=0, 
+            command=self.on_slider_changed
+        )
         slider.bind("<ButtonRelease-1>", self.on_slider_changing_complete)
-
         slider.pack(fill='both', side='left', expand=True)
-
+        
+        self.btn_slider_right = ttk.Button(
+            frm, 
+            text=">", 
+            command=lambda: self.move_video_pos(self.FRAME_CHANGE_DELTA)
+        )
+        self.btn_slider_right.pack(side='left', fill='x')
+        return frm
+    
+    def create_playpause_frame(self, root) -> tk.Frame:
+        frm = tk.Frame(root)
         btn_play = ttk.Button(frm, text="Play", command=self.play_video)
         btn_pause = ttk.Button(frm, text="Pause", command=self.pause_video)
-
-        btn_play.pack(side='left')
-        btn_pause.pack(side='left')
+        btn_play.pack(side='left', fill='both', expand=True)
+        btn_pause.pack(side='right', fill='both', expand=True)
         return frm
     
     def on_slider_changed(self, value: str):
         frame_n = int(round(float(value)))
         if self.frame_change_listener is not None:
             self.frame_change_listener.on_video_frame_change(frame_n)
+            self.frame_n_label.config(text=str(frame_n))
 
     def on_slider_changing_complete(self, event):
         frame_n = int(round(float(self.slider.get())))
         if self.frame_change_listener is not None and not self.playing:
             self.frame_change_listener.on_video_frame_change_complete(frame_n)
+            self.frame_n_label.config(text=str(frame_n))
         
     def play_video(self):
         if self.playing: return
         if self.controller.video_capture is None: return 
-        # Иначе всё работает
+        # Иначе всё работает, несмотря на ошибки
         delay = int(1000/self.controller.get_video_fps())
         
         def play():
@@ -78,12 +117,36 @@ class VideoFrame(tk.Frame):
                 self.pause_video()
                 return
             self.canvas.set_image(img)
-            self.slider.config(value=self.controller.get_video_frame_n())
+            self.update_frame_n_info()
             self.master.after(delay, play)
-        self.slider.config(state="disabled")
+        self.disable_control()
         self.playing = True
         play()
 
     def pause_video(self):
-        self.slider.config(state="normal")
         self.playing = False
+        self.enable_control()
+
+    def update_frame_n_info(self):
+        if self.controller.get_video_frame_n() is None: return
+        self.frame_n_label.config(text=str(self.controller.get_video_frame_n()))
+        self.slider.config(value=self.controller.get_video_frame_n())
+    
+    def set_video_pos(self, pos: int):
+        pos = max(0, min(pos, self.frames_n-1))
+        self.slider.config(value=pos)
+        self.on_slider_changing_complete(None)
+    
+    def move_video_pos(self, delta: int):
+        pos = int(round(float(self.slider.get())))
+        self.set_video_pos(pos+delta)
+    
+    def disable_control(self):
+        self.slider.config(state='disabled')
+        self.btn_slider_left.config(state='disabled')
+        self.btn_slider_right.config(state='disabled')
+    
+    def enable_control(self):
+        self.slider.config(state='normal')
+        self.btn_slider_left.config(state='normal')
+        self.btn_slider_right.config(state='normal')
