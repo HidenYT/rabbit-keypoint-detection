5e4ccf94d9d9c25c46c0afda8fc59413b5adc8d4
HidenYT <buharev2009@gmail.com>
2024-01-25 13:17:21 +0300

    Added extension validator to the inference video model

--- web_app/backend/video_manager/models.py
+++ web_app/backend/video_manager/models.py
@@ -4,15 +4,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from utils.file_uploads import default_upload_path
+from django.core.validators import FileExtensionValidator
 
 def inference_video_upload_path(instance: "InferenceVideo", filename: str) -> str:
     return default_upload_path(settings.INFERENCE_VIDEO_UPLOAD_DIR, instance.user, filename)
 
 class InferenceVideo(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
-    file = models.FileField(upload_to=inference_video_upload_path)
+    file = models.FileField(upload_to=inference_video_upload_path,
+                            validators=[
+                                FileExtensionValidator(["mp4", "avi", "mov"]),
+                            ])
 
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
